f4a374aed82535fc69439c522dc1e1fc2d6c410e
BoyanK95 <boyan_k_95@abv.bg>
2023-05-07 00:10:35 +0300

    feat: Added metadata and global font styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,9 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Ресторант Бюфет',
-  description: 'Най-вкусното място за класни ястия в Стара Загора',
-}
+import './globals.css';
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang='bg'>
+            <body>{children}</body>
+        </html>
+    );
 }
